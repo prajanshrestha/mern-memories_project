089a693fe40ebe37753b34d04f3ed4c55f873fde
Prajan Shrestha <prajan@mavorion.com>
2022-09-08 18:44:32 +0545

    committed

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 // db => mongodb atlas
-const CONNECTION_URL = "mongodb+srv://admin:Prajan32145@cluster0.xchjdbx.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://admin:KmpCyxBqDiSzs4A1@cluster0.xchjdbx.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
